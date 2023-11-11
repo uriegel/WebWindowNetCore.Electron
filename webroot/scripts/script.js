b60364bfdc291253a06a7c6519091f2ec8afb680
Uwe Riegel <uriegel@hotmail.de>
2023-11-11 17:03:54 +0100

    file drop

--- webroot/scripts/script.js
+++ webroot/scripts/script.js
@@ -4,6 +4,20 @@ source.onmessage = (event) => console.log("SSE event", event.data)
 const btn1 = document.getElementById("button")
 const btn2 = document.getElementById("button2")
 const btnDevTools = document.getElementById("buttonDevTools")
+const dropZone = document.getElementById("dropZone")
+
+dropZone.addEventListener("dragover", e => {
+    e.preventDefault()
+    e.stopPropagation()
+})
+
+dropZone.addEventListener("drop", e => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    let pathes = Array.from(e.dataTransfer.files).map(f => f.path)
+    console.log("pathes", pathes)
+})
 
 btnDevTools.onclick = () => webViewShowDevTools()
 
